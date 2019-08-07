7e9a48870047a4decb06f2782f94b6b9a970d8d4
Corentin Cadiou <contact@cphyc.me>
2019-08-07 15:51:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 __author__ = 'Konstantin Weddige'
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='Miniball',
     version='0.2',
     description='Smallest Enclosing Balls of Points',
+    long_description=long_description,
     author='Bernd Gärtner, Konstantin Weddige',
+    url='https://github.com/weddige/miniball',
     packages=['miniball',],
     ext_modules=[
         Extension(
             'miniball.bindings',
-            ['src/miniballmodule.cpp', ],
+            ['src/miniballmodule.cpp'],
+            include_dirs=['src'],
             language='c++',
         ),
+    ],
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
     ],
 )
